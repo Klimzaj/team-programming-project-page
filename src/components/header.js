2a85b332aaa74af49a00b30ea810351e286f0972
Klimzaj <klimzaj@gmail.com>
2018-10-18 13:58:25 +0200

    sample colors

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,33 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
+const MainWrapper = styled.div`
+  background: #E9FFFB  ;
+  margin-bottom: 1.45 rem;
+`
+const ContentWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+const StyledH2 = styled.h2`
+  margin: 0px;
+`
+const StyledLink = styled(Link)`
+  color: #D644A1  ;
+  text-decoration: none;
+`
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+  <MainWrapper>
+    <ContentWrapper>
+      <StyledH2>
+        <StyledLink>
           {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+        </StyledLink>
+      </StyledH2>
+    </ContentWrapper>
+  </MainWrapper>
 )
 
 export default Header
