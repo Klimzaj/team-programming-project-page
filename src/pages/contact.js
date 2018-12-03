e056d977bf0ffb9ffcf0178089bca3e6c7018368
unknown <p.wolski1000@gmail.com>
2018-12-03 19:58:33 +0100

    changing font size and weight, commenting some unnecessary lines of code

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,15 @@
 import React from 'react'
-// import { Link } from 'gatsby'
 import Recaptcha from 'react-google-recaptcha'
 import Layout from '../components/Layout'
 import styled from 'styled-components'
 
-const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY
+//const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY
 
 const ContactWrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
+  color: #737373;
+  font-size: 1rem;
   width: 80%;
   @media (min-width: 768px) {
     width: 75%;
@@ -20,9 +21,9 @@ const ContactWrapper = styled.div`
 const Form = styled.form`
 `
 const FormP = styled.p`
-  font-weight: 700;
-  font-size: inherit;
-  line-height: 1.3;
+  /* font-weight: 400; */
+  /* font-size: inherit; */
+  /* line-height: 1.3; */
   margin-bottom: 10px;
 `
 const BorderInput = styled.input`
@@ -52,10 +53,10 @@ const SubmitButton = styled.input`
   margin-left: auto;
   margin-right: auto;
   padding: 13px 29px;
-  line-height: 17px;
-  font-size: 14px;
+  /* line-height: 17px; */
+  /* font-size: 14px; */
   border: none;
-  font-family: 'Source Sans Pro', sans-serif;
+  /* font-family: 'Source Sans Pro', sans-serif; */
   background: #ffe600;
   margin-top: 33px;
   border-radius: 20px;
@@ -78,33 +79,33 @@ class Contact extends React.Component {
     }
   }
   
-  encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
-  onChangeInput = e => {
-    const { name, value } = e.target
-    this.setState(prevState => ({ ...prevState, [name]: value }))
-  }
-  handleSubmit = e => {
-    if(this.grecaptcha.getResponse() !== 0 && this.grecaptcha.getResponse() !== '' )
-    {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: this.encode({ "form-name": "contact", ...this.state })
-      })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error))
+  // encode = (data) => {
+  //   return Object.keys(data)
+  //       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+  //       .join("&");
+  // }
+  // onChangeInput = e => {
+  //   const { name, value } = e.target
+  //   this.setState(prevState => ({ ...prevState, [name]: value }))
+  // }
+  // handleSubmit = e => {
+  //   if(this.grecaptcha.getResponse() !== 0 && this.grecaptcha.getResponse() !== '' )
+  //   {
+  //     fetch("/", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //       body: this.encode({ "form-name": "contact", ...this.state })
+  //     })
+  //     .then(() => alert("Success!"))
+  //     .catch(error => alert(error))
 
-      e.preventDefault()
+  //     e.preventDefault()
 
-    } else {
-      alert("Please complete the recaptcha validation to continue.")
-      e.preventDefault()
-    }
-  }
+  //   } else {
+  //     alert("Please complete the recaptcha validation to continue.")
+  //     e.preventDefault()
+  //   }
+  // }
   render() {
     const {firstname, lastname, email, message} = this.state
     return (
@@ -114,8 +115,8 @@ class Contact extends React.Component {
           <Form 
             name="contact"
             method="POST"
-            onSubmit={this.handleSubmit}
-            data-netlify="true" 
+            //onSubmit={this.handleSubmit}
+            //data-netlify="true" 
           >
             <input type="hidden" name="form-name" value="contact" />
             <FormP>Imię</FormP>
@@ -123,24 +124,24 @@ class Contact extends React.Component {
               type='text'
               name='firstname'
               value={firstname}
-              onChange={this.onChangeInput}
-              required
+              //onChange={this.onChangeInput}
+              //required
             />
             <FormP>Nazwisko</FormP>
             <LastNameInput
               type='text'
               name='lastname'
               value={lastname}
-              onChange={this.onChangeInput}
-              required
+              //onChange={this.onChangeInput}
+              //required
             />
             <FormP>Email</FormP>
             <EmailInput
               type='email'
               name='email'
               value={email}
-              onChange={this.onChangeInput}
-              required
+              //onChange={this.onChangeInput}
+              //required
             />
             <FormP>Wiadomość</FormP>
             <MessageInput
@@ -148,14 +149,14 @@ class Contact extends React.Component {
               name='message'
               rows="4"   
               value={message}
-              onChange={this.onChangeInput}
-              required
+              //onChange={this.onChangeInput}
+              //required
             />
-            <StyledRecaptcha
+            {/* <StyledRecaptcha
               ref="recaptcha"
               sitekey={RECAPTCHA_KEY}
               required
-            />
+            /> */}
             <SubmitButton
               type='submit'
               value='Submit'
