264a07843f21b45817f1bdd64f1a0c3441a3880a
piotr Wolski <p.wolski1000@gmail.com>
2018-12-18 19:33:15 +0100

    adding myprofile page,

---
+++ src/pages/myprofile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import * as Cookie from '../components/Layout'
+
+class MyProfile  extends React.Component{
+    render(){
+        return(
+            <Layout>
+                <h1>Welcome, [username here]</h1>
+
+            </Layout>
+        )
+    }
+}
+
+export default MyProfile
