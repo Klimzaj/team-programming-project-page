9858c10470057b6b9b647d2a14f75fbd67b817e3
piotr Wolski <p.wolski1000@gmail.com>
2018-12-18 19:32:42 +0100

    logout page, which redirect right after mount to home

---
+++ src/pages/logout.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import * as Cookie from '../components/Cookie/'
+
+const windowGlobal = typeof window !== 'undefined' && window
+
+
+class Logout extends React.Component{
+    UNSAFE_componentWillMount() {
+        if(windowGlobal){
+            Cookie.deleteCookie('access')
+            Cookie.deleteCookie('refresh')
+            // localStorage.removeItem('access')
+            // localStorage.removeItem('refresh')
+            // if(windowGlobal){
+                window.location.replace("http://localhost:8000/");
+            // }
+        }
+    }
+    UNSAFE_componentDidMount(){
+    }
+
+    render(){
+        return(
+            <Layout>
+            <h1>Logged out.</h1>
+            <p>You may now close our website safely.</p>
+            </Layout>
+        )
+    }
+} 
+
+export default Logout
