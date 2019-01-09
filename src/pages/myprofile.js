15d7ebf76f0cbabfaefcbdab6c60a925b4e2a4e8
piotr Wolski <p.wolski1000@gmail.com>
2019-01-09 17:22:55 +0100

    little fixes

--- src/pages/myprofile.js
+++ src/pages/myprofile.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import * as Cookie from '../components/Layout'
+
+const windowGlobal = typeof window !== 'undefined' && window
 
 class MyProfile  extends React.Component{
+
+    UNSAFE_componentWillMount = () => {
+        if(windowGlobal && localStorage.getItem('access'))
+          window.location.replace("http://localhost:8000/login")      
+    }
+
     render(){
         return(
             <Layout>
                 <h1>Welcome, [username here]</h1>
-
             </Layout>
         )
     }
