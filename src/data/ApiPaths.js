59b39d78110e0c2ef020d67f371e0114d044c103
piotr Wolski <p.wolski1000@gmail.com>
2019-01-11 21:52:50 +0100

    adding a / to endpoint for register

--- src/data/ApiPaths.js
+++ src/data/ApiPaths.js
@@ -1,5 +1,5 @@
 export const domainName = 'https://cukierpuder.pythonanywhere.com/' 
 
-export const registerPath = 'https://cukierpuder.pythonanywhere.com/auth/register'
+export const registerPath = 'https://cukierpuder.pythonanywhere.com/auth/register/'
 
 export const loginPath = 'https://cukierpuder.pythonanywhere.com/auth/token/obtain/'
