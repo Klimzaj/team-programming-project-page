b1ae19ff664d6ae827585e528ddae652fcc1ba95
Piotr Wolski <p.wolski1000@gmail.com>
2019-01-12 13:37:42 +0100

    chanigng url to redirect to

--- src/pages/myprofile.js
+++ src/pages/myprofile.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
-
+import * as paths from '../data/ApiPaths'
 const windowGlobal = typeof window !== 'undefined' && window
 
 class MyProfile  extends React.Component{
 
     UNSAFE_componentWillMount = () => {
         if(windowGlobal && localStorage.getItem('access'))
-          window.location.replace("http://localhost:8000/login")      
+          window.location.replace(paths.dommainName + '/login')      
     }
 
     render(){
