184fb44ac4bf93aafaa6bf33412fc14ae322dd42
piotr Wolski <p.wolski1000@gmail.com>
2019-01-22 03:07:49 +0100

    src/data/ApiPaths.js

--- src/data/ApiPaths.js
+++ src/data/ApiPaths.js
@@ -8,3 +8,7 @@ export const loginPath = '/auth/token/obtain/'
 //recipes
 export const addRecipePath = '/api/recipes/'
 export const searchRecipePath = '/api/recipes/'
+
+
+//tesco-api
+export const getProductPath = '/tesco-api/grocery-search/'
