257e7a29e0dbffa622f933ec85d1fdfd0e1cd2cc
piotr Wolski <p.wolski1000@gmail.com>
2019-01-22 04:06:46 +0100

    removing coma(maybe this is gonna work) from headers after last header in json

--- src/pages/addrecipe.js
+++ src/pages/addrecipe.js
@@ -129,10 +129,12 @@ class AddRecipe extends React.Component{
 
       if(windowGlobal){
         console.log('thats my acces token: ', localStorage.getItem('access'))
+      
+        // axios.post('http://localhost:8000/tesco-api/grocery-search/', mData, {      
         axios.post(paths.domainName+paths.getProductPath, mData, {
         headers: {
             'Content-Type':'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('access'),
+            'Authorization': 'Bearer ' + localStorage.getItem('access')
         }
         }).then(function (response) {
         //handle success
