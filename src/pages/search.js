7873f81431ce5a5e806d856762f50d82e0446078
Klimzaj <klimzaj@gmail.com>
2019-01-28 23:33:09 +0100

    all recipe

--- src/pages/search.js
+++ src/pages/search.js
@@ -49,7 +49,7 @@ class Search extends Component{
   
 
   handleSubmit = e => {
-    e.preventDefault()
+    // e.preventDefault()
     //here request for a specific recipe
   if(windowGlobal)
     axios.get(paths.domainName+paths.searchRecipePath, 
