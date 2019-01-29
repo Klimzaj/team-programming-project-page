11ed9cc76cd372d9c90a869f8b7416a82fcd5ece
Klimzaj <klimzaj@gmail.com>
2019-01-29 11:52:11 +0100

    take details test

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -13,11 +13,8 @@ const Recipe = ({name, description, image, data2}) =>
 {
     let price = 0
     let nameArray = []
-    console.log(data2)
     console.log(typeof data2)
-    console.log(data2[0])
-    console.log(data2.o)
-    console.log(data2.O)
+    console.log(data2["0"])
     console.log(data2)
     if(data2)
     {
@@ -27,6 +24,12 @@ const Recipe = ({name, description, image, data2}) =>
             // price += el.price
             // nameArray.push(el.name)
         }
+    }
+    if(data2)
+    {
+        data2.map((el,i ) => {
+            console.log(el)
+        })
     }
     
 return(
