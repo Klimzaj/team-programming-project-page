b2bfb0d30cafd2cbdf53037bb8625db57a1f858b
piotr Wolski <p.wolski1000@gmail.com>
2019-02-02 01:56:40 +0100

    changing language to english

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -3,7 +3,7 @@ import * as El from './styles'
 
 const Features = ({item}) => (
     <El.MainWrapper>
-        <El.Title>Czas to pieniądz</El.Title>
+        <El.Title>Time is priceless</El.Title>
         <El.Underline></El.Underline>
         
         <div styles={{width: '250px', height: '10px', backgroundColor: 'red'}} ></div>
